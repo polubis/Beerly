0bce2c737f74ac902c50c435ac36a53681401774
polubis <polubis@wp.pl>
2019-06-28 21:09:19 +0200

    Create index files and configure sass, css loaders

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,19 +3,27 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
         test: /\.html$/,
         use: [
           {
             loader: "html-loader"
           }
         ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       }
     ]
   },
