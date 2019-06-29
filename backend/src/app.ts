a1567e860b44f2914abf0e6db06c1973311a1656
polubis <polubis@wp.pl>
2019-06-29 09:36:55 +0200

    Adding inversify, typeorm and fix backend running

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,10 @@ import express = require('express');
 
 const app: express.Application = express();
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World!");
+app.get('/', (req, res, next) => {
+  res.send('Hello World!');
 });
 
 app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+  console.log('Example app listening on port 3000!');
 });
