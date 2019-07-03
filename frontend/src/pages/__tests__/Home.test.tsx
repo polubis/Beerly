1e95100ba1303b43750056818953d6cd03696a64
polubis <polubis@wp.pl>
2019-07-03 19:18:40 +0200

    Move setupTests to root folder

--- frontend/src/pages/__tests__/Home.test.tsx
+++ frontend/src/pages/__tests__/Home.test.tsx
@@ -5,7 +5,8 @@ import Home from '../home/Home';
 
 describe('First React component test with Enzyme', () => {
   it('renders without crashing', () => {
-    shallow(<Home />);
+    const result = shallow(<Home />).contains(<div id="home-page" />);
+    expect(result).toBeTruthy();
   });
 });
 
