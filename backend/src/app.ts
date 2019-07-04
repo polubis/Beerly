1415cd62bc069f3ad4d214eee7795c533e846677
polubis <polubis@wp.pl>
2019-07-04 16:48:42 +0200

    Add object oriented app and server files implementation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,23 @@
-import express = require('express');
+import express, { Application } from 'express';
+import bodyParser from 'body-parser';
 
-const app: express.Application = express();
+export default class App {
+  private async init() {
+    const app: Application = express();
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World!');
-});
+    app.set('port', process.env.PORT || 3000);
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
+    return Promise.resolve(app);
+  }
+
+  public async start() {
+    const app = await this.init();
+    const server = app.listen(app.get('port'), async () => {
+      console.log(`Service running at port ${app.get('port')} in ${app.get('env')} mode`);
+      console.log('Date: ', new Date().toDateString());
+    });
+    return Promise.resolve(server);
+  }
+}
---
+++ backend/src/server.ts
@@ -0,0 +1,4 @@
+import App from './app';
+
+module.exports = new App().start();
+
