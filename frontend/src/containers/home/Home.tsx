af7e5680e4d02352bda4b93ad071648e8a5c7634
danielcudnik <danielcudnik93@gmail.com>
2019-07-26 18:53:04 +0200

    add absolute imports configuration, fix compilation error

--- frontend/src/containers/home/Home.tsx
+++ frontend/src/containers/home/Home.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import Navbar from '../../components/home/navbar/navbar';
-
 import classes from './Home.scss';
 
 class Home extends React.Component<any, any> {
   render() {
     return (
       <div id="home-page" className={classes.home}>
-        <Navbar />
         Home Page
       </div>
     );
